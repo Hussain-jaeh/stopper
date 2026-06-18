13bd6ea07f6e59c80a524785257f856730e4b1f5
hussain-jaeh <hussaijaeh320@gmail.com>
2026-06-18 06:06:46 +0100

    fix: simplify metro.config.js
    Remove monorepo resolution since we don't need to resolve from parent directory.
    All Convex files are now included locally.

--- stopper-app/metro.config.js
+++ stopper-app/metro.config.js
@@ -1,16 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
-const projectRoot = __dirname;
-const monorepoRoot = path.resolve(projectRoot, '..');
-
-const config = getDefaultConfig(projectRoot);
-
-// Let Metro watch and resolve modules from the parent directory (convex/)
-config.watchFolders = [monorepoRoot];
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(monorepoRoot, 'node_modules'),
-];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
